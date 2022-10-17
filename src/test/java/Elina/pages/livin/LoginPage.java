fceda024e78862c2962207d936dc063e7b5867f2
Nikita <elina.cerniavskaja@gmail.com>
2022-10-17 16:43:25 +0300

    Add new method readMessage

--- src/test/java/Elina/pages/livin/LoginPage.java
+++ src/test/java/Elina/pages/livin/LoginPage.java
@@ -1,19 +1,39 @@
 package Elina.pages.livin;
 
 import Elina.pages.Common;
+import Elina.pages.Locators;
 
 public class LoginPage {
-    public static void setup() {
-        Common.setUp();
-
-    }
-
     public static void open(String url) {
         Common.openUrl(url);
-
     }
 
-    public static void close() {
-        Common.close();
+    //public static void sleep(int millis) {
+        //Common.sleep(millis);
+   // }
+
+    public static void enterElPastas(String expectedElPastas) {
+        Common.sendKeysToElement(
+                expectedElPastas,
+                Locators.Livin.Login.inputElPastas
+        );
     }
+
+    public static void enterSlaptazodis(String expectedSlaptazodis) {
+        Common.sendKeysToElement(
+                expectedSlaptazodis,
+                Locators.Livin.Login.inputSlaptazodis
+        );
+    }
+
+    public static void clickBottomPrisijungti() {
+        Common.clickElement(
+                Locators.Livin.Login.buttonPrisijungti);
+    }
+
+    public static String readMessageElPastas() {
+        return Common.getElementText(
+                Locators.Livin.Login.inputElPastas);
+    }
+
 }
