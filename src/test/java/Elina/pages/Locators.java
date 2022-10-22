d30ae664b7ac9151b566ac6c40516c187aae8117
Nikita <elina.cerniavskaja@gmail.com>
2022-10-22 19:51:53 +0300

    Add new xpath to Locators class

--- src/test/java/Elina/pages/Locators.java
+++ src/test/java/Elina/pages/Locators.java
@@ -9,20 +9,24 @@ public class Locators {
             public static By inputEmail = By.xpath("//*[@id='username']");
             public static By inputPassword = By.xpath("//input[@id='password']");
             public static By buttonSubmit = By.xpath("//button[@class='woocommerce-Button button']");
-            public static By headerManoPaskyra = By.xpath("//*[text()='elina.cerniavskaja@gmail.com']");
+            public static By headerManoPaskyra = By.xpath("//*[@class='header__upper__icons']");
             public static By loginError = By.xpath("//*[@class='woocommerce-error']");
-
+            public static By userInformation = By.xpath("//*[text()='Vartotojo informacija']");
+            public static By inputUserName = By.xpath("//*[@id='account_first_name']");
+            public static By inputUserLastName = By.xpath("//*[@id='account_last_name']");
+            public static By buttonSubmitNotification =By.xpath("//*[@class='btn btn--full']");
+            public static By messageOfNotification = By.xpath("//div[@class='woocommerce-message']");
         }
 
         public static class SearchProduct {
-            public static By searchButton = By.xpath("//*[text()='dgwt-wcas-search-input-1']");
-            public static By inputProduct = By.xpath("//*[@class='js-dgwt-wcas-enable-mobile-form dgwt-wcas-enable-mobile-form']");
-            public static By searchProduct = By.xpath("//*[@class='text--s text--bold']");
-            public static By ProductFromMainPage = By.xpath("//*[text()='Produktai']");
+            public static By searchButton = By.xpath("(//div[@class='header__lower__right sm-hide']//span)[1]");
+            public static By inputProduct = By.xpath("//input[@id='dgwt-wcas-search-input-1']");
+            public static By searchProduct = By.xpath("//div[@class='dgwt-wcas-st']");
+            public static By searchResult = By.xpath("//span[text()='Brazil Santos']");
+            public static By productFromMainPage = By.xpath("//*[text()='Produktai']");
             public static By selectedProductFromMainPage = By.xpath("//*[text()='Raffaello']");
-            public static By cartButon = By.xpath("//*[@class='single_add_to_cart_button button alt']");
-
-            public static By MessageFromCart = By.xpath("//*[@class='woocommerce-message']");
+            public static By cartButton = By.xpath("//*[@class='single_add_to_cart_button button alt']");
+            public static By messageFromCart = By.xpath("//*[@class='product_title entry-title']");
         }
     }
 }
